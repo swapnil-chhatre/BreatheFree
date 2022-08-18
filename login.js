5787bc129a9d62e6131808e8372a2c58463c10bf
Swapnil Chhatre <swapnilchhatre10@gmail.com>
2022-08-18 10:42:21 +0530

    Upscaling V1

---
+++ login.js
@@ -0,0 +1,22 @@
+// function login(form){
+//     var usr = form.Username.value;
+//     var pwd = form.Password.value;
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.open("post", "Login", true);
+//     xhr.onreadystatechange = function(){
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             loginResults();
+//         }
+//     }
+// }
+
+// window.addEventListener(window,"load", function() {
+//     var loginForm = document.getElementById("LoginForm");
+//     window.addEventListener(loginForm, "submit", function() {
+//          login(loginForm);
+//      });
+//  });
+
+
+    
